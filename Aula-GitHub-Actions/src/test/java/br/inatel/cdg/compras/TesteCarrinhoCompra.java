a75bea2f5b68b61090d8560cc5534868d739caa3
Marcos1Guerra <marcos.guerra@ges.intel.br>
2024-09-25 19:10:36 -0300

    teste2

--- Aula-GitHub-Actions/src/test/java/br/inatel/cdg/compras/TesteCarrinhoCompra.java
+++ Aula-GitHub-Actions/src/test/java/br/inatel/cdg/compras/TesteCarrinhoCompra.java
@@ -6,74 +6,68 @@ import org.junit.Test;
 
 public class TesteCarrinhoCompra {
 
+    // Teste inicial básico (Padrão 1)
+    @Test
+    public void testSomaTotalCompra() throws NumeroNegativoException {
+        Brownie bw1 = new Brownie("Brownie Nutella", 6, 150); // 6 * 150 = 900
+        Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100); // 4 * 100 = 400
 
-	// Teste inicial básico (Padrão 1)
-	@Test
-	public void testSomaTotalCompra() throws NumeroNegativoException {
+        CarrinhoCompra carrinho = new CarrinhoCompra();
 
-		Brownie bw1 = new Brownie("Brownie Nutella", 6, 150);
-		Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100);
+        carrinho.adiciona(bw1);
+        carrinho.adiciona(bw2);
 
-		CarrinhoCompra carrinho = new CarrinhoCompra();
+        assertEquals(1300, carrinho.somaTotal(), 0.01); // Total should be 1300
+    }
 
-		carrinho.adiciona(bw1);
-		carrinho.adiciona(bw2);
+    // Teste inicial básico (Padrão 1) + algo diferente (teste valor total de itens)
+    // (Padrão 2)
+    @Test
+    public void testSomaTotalQtd() throws NumeroNegativoException {
 
-		assertEquals(250, carrinho.somaTotal(),0.01);
+        Brownie bw1 = new Brownie("Brownie Nutella", 6, 150);
+        Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100);
 
-	}
+        CarrinhoCompra carrinho = new CarrinhoCompra();
 
+        carrinho.adiciona(bw1);
+        carrinho.adiciona(bw2);
+        assertEquals(10, carrinho.somaTotalItens(), 0.01);
 
-	// Teste inicial básico (Padrão 1) + algo diferente (teste valor total de itens) (Padrão 2)
-	@Test
-	public void testSomaTotalQtd() throws NumeroNegativoException {
+    }
 
-		Brownie bw1 = new Brownie("Brownie Nutella", 6, 150);
-		Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100);
+    // Teste negativo (Padrão 3) forçando uma situação inesperada
+    @Test(expected = NumeroNegativoException.class)
+    public void testSomaNegativo() throws NumeroNegativoException {
 
-		CarrinhoCompra carrinho = new CarrinhoCompra();
+        Brownie bw1 = new Brownie("Brownie Nutella", -6, 150);
+        Brownie bw2 = new Brownie("Brownie Doce de Leite", -4, 100);
 
-		carrinho.adiciona(bw1);
-		carrinho.adiciona(bw2);
-		assertEquals(10, carrinho.somaTotalItens(),0.01);
+        CarrinhoCompra carrinho = new CarrinhoCompra();
 
+        carrinho.adiciona(bw1);
+        carrinho.adiciona(bw2);
 
-	}
+    }
 
-	// Teste negativo (Padrão 3) forçando uma situação inesperada
-	@Test(expected = NumeroNegativoException.class)
-	public void testSomaNegativo() throws NumeroNegativoException {
+    // Teste integração (Padrão 4) 2 classes (Carrinho e Cupom sendo testadas
+    // juntas).
+    @Test
+    public void testIntegracaoCupom() throws NumeroNegativoException {
+        Brownie bw1 = new Brownie("Brownie Nutella", 6, 150); // 6 * 150 = 900
+        Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100); // 4 * 100 = 400
 
-		Brownie bw1 = new Brownie("Brownie Nutella", -6, 150);
-		Brownie bw2 = new Brownie("Brownie Doce de Leite", -4, 100);
+        CarrinhoCompra carrinho = new CarrinhoCompra();
+        CupomDesconto cupom = new CupomDesconto(0.20);
+        carrinho.setCupom(cupom);
 
-		CarrinhoCompra carrinho = new CarrinhoCompra();
+        carrinho.adiciona(bw1);
+        carrinho.adiciona(bw2);
 
-		carrinho.adiciona(bw1);
-		carrinho.adiciona(bw2);
+        assertEquals(1040, carrinho.somaTotal(), 0.1); // Total com desconto de 20%
+    }
 
-	}
-
-	// Teste integração (Padrão 4) 2 classes (Carrinho e Cupom sendo testadas juntas).
-	@Test
-	public void testIntegracaoCupom() throws NumeroNegativoException {
-
-		Brownie bw1 = new Brownie("Brownie Nutella", 6, 150);
-		Brownie bw2 = new Brownie("Brownie Doce de Leite", 4, 100);
-
-		CarrinhoCompra carrinho = new CarrinhoCompra();
-		CupomDesconto cupom = new CupomDesconto(0.20);
-		carrinho.setCupom(cupom);
-
-		carrinho.adiciona(bw1);
-		carrinho.adiciona(bw2);
-		carrinho.somaTotal();
-
-		assertEquals(200, carrinho.somaTotal(), 0.1);
-
-	}
-
-	// Teste inicial básico (Padrão 5) verificando o desconto no cupom sem itens
+    // Teste inicial básico (Padrão 5) verificando o desconto no cupom sem itens
     @Test
     public void testCupomSemItens() throws NumeroNegativoException {
         CarrinhoCompra carrinho = new CarrinhoCompra();
@@ -83,23 +77,24 @@ public class TesteCarrinhoCompra {
         assertEquals(0, carrinho.somaTotal(), 0.01);
     }
 
-    // Teste de comportamento (Padrão 6) carrinho com múltiplos itens e diferentes quantidades
+    // Teste de comportamento (Padrão 6) carrinho com múltiplos itens e diferentes
+    // quantidades
     @Test
     public void testCarrinhoMultiplosItens() throws NumeroNegativoException {
-        Brownie bw1 = new Brownie("Brownie Nutella", 2, 150);
-        Brownie bw2 = new Brownie("Brownie Doce de Leite", 3, 100);
-        Brownie bw3 = new Brownie("Brownie Chocolate", 5, 200);
-
+        Brownie bw1 = new Brownie("Brownie Nutella", 2, 150);  // 2 * 150 = 300
+        Brownie bw2 = new Brownie("Brownie Doce de Leite", 3, 100);  // 3 * 100 = 300
+        Brownie bw3 = new Brownie("Brownie Chocolate", 5, 200);  // 5 * 200 = 1000
+    
         CarrinhoCompra carrinho = new CarrinhoCompra();
-
+    
         carrinho.adiciona(bw1);
         carrinho.adiciona(bw2);
         carrinho.adiciona(bw3);
+    
+        assertEquals(1600, carrinho.somaTotal(), 0.01);  // Total = 300 + 300 + 1000 = 1600
+    }    
 
-        assertEquals(1350, carrinho.somaTotal(), 0.01);
-    }
-
-	// Teste (Padrão 7) verificando a adição de vários itens iguais
+    // Teste (Padrão 7) verificando a adição de vários itens iguais
     @Test
     public void testAdicionaItensIguais() throws NumeroNegativoException {
         Brownie bw1 = new Brownie("Brownie Nutella", 5, 100);
@@ -112,7 +107,8 @@ public class TesteCarrinhoCompra {
         assertEquals(1000, carrinho.somaTotal(), 0.01);
     }
 
-	// Teste inicial básico (Padrão 8) verificando a quantidade total de itens após remoção
+    // Teste inicial básico (Padrão 8) verificando a quantidade total de itens após
+    // remoção
     @Test
     public void testRemoveItemQtd() throws NumeroNegativoException {
         Brownie bw1 = new Brownie("Brownie Nutella", 6, 150);
@@ -141,7 +137,8 @@ public class TesteCarrinhoCompra {
         assertEquals(1000, carrinho.somaTotalItens(), 0.01);
     }
 
-    // Teste de comportamento (Padrão 10) verificando o carrinho após resetar (esvaziar)
+    // Teste de comportamento (Padrão 10) verificando o carrinho após resetar
+    // (esvaziar)
     @Test
     public void testCarrinhoReset() throws NumeroNegativoException {
         Brownie bw1 = new Brownie("Brownie Nutella", 5, 150);
@@ -158,4 +155,3 @@ public class TesteCarrinhoCompra {
     }
 
 }
-
